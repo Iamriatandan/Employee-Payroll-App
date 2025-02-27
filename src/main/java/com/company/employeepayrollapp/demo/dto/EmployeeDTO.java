54182d8d38ad08e82d143c24ce6fde2e3a613ca3
iamriatandan <tandanria@gmail.com>
2025-02-27 13:31:11 +0530

    Ria Added . UC6 Lombok and Dto changes

--- src/main/java/com/company/employeepayrollapp/demo/dto/EmployeeDTO.java
+++ src/main/java/com/company/employeepayrollapp/demo/dto/EmployeeDTO.java
@@ -1,21 +1,15 @@
 package com.company.employeepayrollapp.demo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmployeeDTO {
     private String name;
     private double salary;
-
-    // Constructors
-    public EmployeeDTO() {}
-
-    public EmployeeDTO(String name, double salary) {
-        this.name = name;
-        this.salary = salary;
-    }
-
-    // Getters and Setters
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public double getSalary() { return salary; }
-    public void setSalary(double salary) { this.salary = salary; }
 }
