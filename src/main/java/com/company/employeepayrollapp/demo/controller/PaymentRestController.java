9a29e000a2931035fe94678c0156567e9e6075fc
iamriatandan <tandanria@gmail.com>
2025-03-07 15:07:33 +0530

    Ria Added . UC7 Logging commands

--- src/main/java/com/company/employeepayrollapp/demo/controller/PaymentRestController.java
+++ src/main/java/com/company/employeepayrollapp/demo/controller/PaymentRestController.java
@@ -1,48 +1,50 @@
 package com.company.employeepayrollapp.demo.controller;
+
 import com.company.employeepayrollapp.demo.model.Employee;
 import com.company.employeepayrollapp.demo.service.EmployeeService;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/employees")
-@Slf4j
 public class PaymentRestController {
+
+    private static final Logger log = LoggerFactory.getLogger(PaymentRestController.class);
+
     @Autowired
     private EmployeeService employeeService;
 
     @PostMapping("/add")
-    public Employee addEmployee(@RequestBody Employee employee){
-        log.info("Addeing new Employee : {} ", employee );
+    public Employee addEmployee(@RequestBody Employee employee) {
+        log.info("Adding new Employee : {} ", employee);
         return employeeService.saveEmployee(employee);
     }
 
     @GetMapping("/all")
-    public List<Employee>getAllEmployees(){
-        log.info("Fetching all employees ");
-        return employeeService.getAllEmployees;
+    public List<Employee> getAllEmployees() {
+        log.info("Fetching all employees");
+        return employeeService.getAllEmployees();
     }
 
     @GetMapping("/{id}")
-    public Employee getEmployeeById(@PathVariable Long id){
+    public Employee getEmployeeById(@PathVariable Long id) {
         log.info("Fetching employee with ID : {}", id);
         return employeeService.getEmployeeById(id);
     }
 
     @PutMapping("/update/{id}")
-    public Employee updateEmployee(@PathVariable Long id , @RequestBody Employee employee){
-        log.info("Updating employee with id : {} ",id);
-        return employeeService.updateEmployee(id,employee);
+    public Employee updateEmployee(@PathVariable Long id, @RequestBody Employee employee) {
+        log.info("Updating employee with ID : {}", id);
+        return employeeService.updateEmployee(id, employee);
     }
 
     @DeleteMapping("/delete/{id}")
-    public String deleteEmployee(@PathVariable Long id){
-        log.warn("Deleting employee with id : {}", id);
+    public String deleteEmployee(@PathVariable Long id) {
+        log.warn("Deleting employee with ID : {}", id);
         return employeeService.deleteEmployee(id);
     }
 }
